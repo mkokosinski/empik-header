ce661c8865c3b5a222ed598c32d812c4bc01b254
mkokosinski <mkokosinski91@gmail.com>
2022-02-06 13:44:58 +0100

    header tests

---
+++ src/components/header/Header.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+
+import Header from './Header';
+
+describe('Button component', () => {
+  test('should render correctly', async () => {
+    render(<Header />);
+    await screen.findByRole('banner');
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+  });
+
+  test('should render tiles correctly', async () => {
+    render(<Header />);
+    expect(screen.getAllByTestId('header-tile').length).toBe(3);
+  });
+});
